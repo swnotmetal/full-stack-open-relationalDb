85f28e52cf91d1cea71cc70edcf625ff330b697c
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2025-04-01 23:21:03 +0300

    13.7, error handling middleware and fancy handling scheme in index.js

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,67 +1,50 @@
 const router = require('express').Router()
-
 const {Blog} = require('../models')
 
 router.get('/', async(req, res) => {
     const blogs = await Blog.findAll()
     console.log(JSON.stringify(blogs, null, 2))
     res.json(blogs)
-  })
+})
 
 router.post('/', async(req, res) => {
-    try {
-        if(!req.body.title && !req.body.url) {
-            return res.status(400).json({ error: "Title and URL is required"})
-        }
-
-        const blog = await Blog.create(req.body)
-        console.log(blog.toJSON());
-        return res.json(blog)
-    }catch(error) {
-        console.log(error)
-        res.status(500).json({ message: 'Internal Server Error' })
+    if(!req.body.title || !req.body.url) { // Changed && to || to require both fields
+        return res.status(400).json({ error: "Title and URL are required"})
     }
+
+    const blog = await Blog.create(req.body)
+    console.log(blog.toJSON())
+    return res.json(blog)
 })
 
 router.delete('/:id', async(req, res) => {
     const {id} = req.params
-
-    try {
-        const blog = await Blog.findByPk(req.params.id)
-        if(blog) {
-            await blog.destroy()
-            res.status(200).json({message: `Blog with ID ${id} deleted`})
-        } else {
-            res.status(404).json({message: `Blog with ID ${id} not found`})
-        }
-    } catch(error) {
-        console.log(error)
-        res.status(500).json({message: 'Internal Server Error'})
+    
+    const blog = await Blog.findByPk(id)
+    if(blog) {
+        await blog.destroy()
+        res.status(200).json({message: `Blog with ID ${id} deleted`})
+    } else {
+        res.status(404).json({message: `Blog with ID ${id} not found`})
     }
-        
 })
 
 router.put('/:id', async(req, res) => {
     const { id } = req.params
     const { likes } = req.body
 
-    try {
-        const blog = await Blog.findByPk(id) //remember this is under sequelize
-        
-        if (!blog) {
-            return res.status(404).json({ message: "No blog found" })
-        }
-
-        if (likes !== undefined) {
-            blog.likes = likes
-            await blog.save()
-        }
-
-        res.json(blog)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: 'Internal Server Error' })
+    const blog = await Blog.findByPk(id)
+    
+    if (!blog) {
+        return res.status(404).json({ message: "No blog found" })
     }
+
+    if (likes !== undefined) {
+        blog.likes = likes
+        await blog.save()
+    }
+
+    res.json(blog)
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
-
+require('express-async-errors')
 
 const blogRouter = require('./controllers/blogs')
 
@@ -11,7 +11,48 @@ app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
 
+app.use((err, req, res, next) => {
+  console.error('Error details:', err)
 
+  if (err.name === 'SequelizeValidationError') {
+    return res.status(400).json({ 
+      error: 'Validation failed', 
+      details: err.errors.map(e => e.message)
+    })
+  }
+
+  if (err.name === 'SequelizeUniqueConstraintError') {
+    return res.status(400).json({ 
+      error: 'Unique constraint violation',
+      details: 'A unique field already has this value'
+    })
+  }
+
+  if (err.name === 'SequelizeConnectionError' || err.name === 'SequelizeConnectionRefusedError') {
+    return res.status(500).json({
+      error: 'Database connection error',
+      details: 'Could not connect to the database'
+    })
+  }
+
+
+  if (err.name === 'TypeError' || err.name === 'SequelizeDatabaseError') {
+    return res.status(400).json({
+      error: 'Invalid data type',
+      details: 'Please check your input values'
+    })
+  }
+
+
+  res.status(500).json({
+    error: 'Something went wrong',
+    details: err.message || 'Unknown error'
+  })
+})
+
+app.use((req, res) => {
+  res.status(404).send({ error: 'Unknown endpoint' })
+})
 const start = async () => {
     await connectToDatabase()
     app.listen(PORT, () => {
